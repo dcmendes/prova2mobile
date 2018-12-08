a13235d935c7185a985f9853c297a3c188495f4a
dcmendes <diegocmendes@msn.com>
2018-12-08 08:11:32 -0200

    implementaçõa da class 'Installment'

--- app/src/main/java/com/example/diego/prova2/Model/Installment.java
+++ app/src/main/java/com/example/diego/prova2/Model/Installment.java
@@ -2,23 +2,49 @@ package com.example.diego.prova2.Model;
 
 public class Installment {
 
-    private int value;
+    private int nInst;
+    private double value;
     private double interest;
+    private double amorization;
+    
 
 
-    public int getValue() {
+    public Installment(int nInst, double value, double interest, double amorization) {
+        this.nInst = nInst;
+        this.value = value;
+        this.interest = interest;
+        this.amorization = amorization;
+    }
+
+    public int getnInst() {
+        return nInst;
+    }
+
+    public void setnInst(int nInst) {
+        this.nInst = nInst;
+    }
+
+    public double getValue() {
         return value;
     }
 
-    public void setValue(int value) {
+    public void setValue(double value) {
         this.value = value;
+    }
+
+    public double getInterest() {
+        return interest;
     }
 
     public void setInterest(double interest) {
         this.interest = interest;
     }
 
-    public double getInterest() {
-        return interest;
+    public double getAmorization() {
+        return amorization;
+    }
+
+    public void setAmorization(double amorization) {
+        this.amorization = amorization;
     }
 }
