75bf2a5f0b9b56e7023318c81eea9d0435a81f5e
dcmendes <diegocmendes@msn.com>
2018-12-10 20:20:12 -0200

    enxugando codigo

--- app/src/main/java/com/example/diego/prova2/View/MainActivity.java
+++ app/src/main/java/com/example/diego/prova2/View/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.diego.prova2.View;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -32,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
     private EditText amountEdit;
     private TextView resultado;
     private SeekBar percentSeekBar;
-    private TextView resultado2;
     private EditText pay;
     private Button button;
 
@@ -48,31 +50,25 @@ public class MainActivity extends AppCompatActivity {
         percentSeekBar = (SeekBar) findViewById(R.id.percentSeekBar);
         percentSeekBar.setOnSeekBarChangeListener(seekBarListener);
         percentFormat.setMinimumFractionDigits(2);
-        resultado2 = (TextView) findViewById(R.id.resultado2);
         resultado.setText(currencyFormat.format(0));
-        amountEdit.addTextChangedListener (resultEditWatcher);
+
         button = (Button)findViewById(R.id.btSim);
         pay = (EditText) findViewById(R.id.nPay);
 
     }
 
-    private void calculate() {
+    public void syncronyzingSeek(){
+        percent = percentSeekBar.getProgress();
 
-        percentTextView.setText(percentFormat.format(percent));
-
-        double tip = billAmount * percent;
-        double total = billAmount + tip;
-
-        resultado.setText(currencyFormat.format((tip)));
-
+        this.percentTextView.setText(percentFormat.format(percent/100));
     }
 
 
    private final SeekBar.OnSeekBarChangeListener seekBarListener = new SeekBar.OnSeekBarChangeListener() {
         @Override
         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-            percent = progress/100.0;
-            calculate();
+
+            syncronyzingSeek();
 
         }
 
@@ -87,79 +83,53 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-
-  private final TextWatcher resultEditWatcher;
-
-    {
-        resultEditWatcher = new TextWatcher() {
-            @Override
-            public void onTextChanged(CharSequence s,
-                                      int i, int i1, int i2) {
-
-                try {
-
-                    billAmount = Double.parseDouble(s.toString()) ;
-                    resultado.setText(currencyFormat.format(billAmount));
-                } catch (NumberFormatException e) {
-
-                    resultado.setText(R.string.enter_amount);
-                    billAmount = 0.0;
-                }
-                calculate();
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-            }
-        };
-    }
-/*
-    public void Simulation(View view){
-
-        if ( amountEdit.getText().length() == 0  || pay.getText().length() == 0 ) {
-            resultado2.setText("Campos vazios");
-        }
-        else {
-            calculate();
-            Double d = Double.parseDouble(amountEdit.getText().toString());
-            Double t = Double.parseDouble(pay.getText().toString());
-
-            m = d * (Math.pow((1 + percent), t));
-            p = m /t;
-            resultado2.setText(String.valueOf(currencyFormat.format(percent)));
-        }
-
-    }*/
-
-
-
 public void simListView(View view) throws ParseException {
 
-    Loan  l = null;
-    NumberFormat currency = NumberFormat.getCurrencyInstance();
-    currency.setGroupingUsed(true);
-    currency.setMinimumFractionDigits(2);
+   if (amountEdit.getText().length() == 0 || pay.getText().length() == 0){
 
-    this.m = 0.0;
-    this.t = Integer.parseInt(pay.getText().toString());
-    this.i  = 0.1;
-    m = Double.parseDouble(amountEdit.getText().toString());
-    i = Double.parseDouble(pay.getText().toString());
+        emptyForm();
 
-    Bundle params = new Bundle();
-    params.putInt("mounth", t);
-    params.putDouble("Amount", m);
-    params.putDouble("Rate Interest", i);
+    } else {
 
-    Intent intent = new Intent(this, AmountView.class);
-    intent.putExtras(params);
-    startActivity(intent);
+        Loan  l = null;
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        currency.setGroupingUsed(true);
+        currency.setMinimumFractionDigits(2);
 
+        this.m = 0.0;
+        this.t = Integer.parseInt(pay.getText().toString());
+        this.i  = 0.1;
+        m = Double.parseDouble(amountEdit.getText().toString());
+        i = Double.parseDouble(pay.getText().toString());
 
+        Bundle params = new Bundle();
+        params.putInt("mounth", t);
+        params.putDouble("Amount", m);
+        params.putDouble("Rate Interest", i);
+
+        Intent intent = new Intent(this, AmountView.class);
+        intent.putExtras(params);
+        startActivity(intent);
+
+   }
 }
+
+    public void emptyForm() {
+        new AlertDialog.Builder(this)
+                .setTitle("Campo vazio")
+                .setMessage("O campo preço está vazio")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        emptySeekBar();
+                    }
+                }).show();
+    }
+
+
+
+        public void emptySeekBar(){
+        percentSeekBar.setProgress(0);
+    }
 
 }
